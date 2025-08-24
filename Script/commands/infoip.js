f9371c766321cfcc8bdfe4080553dab33b82674c
kamrulislam6666 <ruhan3888@gmail.com>
2025-08-24 18:40:21 +0600

    Update infoip.js

--- Script/commands/infoip.js
+++ Script/commands/infoip.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "infoip",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "Tamim Tahrim ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
 	description: "get info of ip address",
   usages: "[ip address]",
 	commandCategory: "...",
