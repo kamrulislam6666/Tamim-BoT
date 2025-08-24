e48f4be1239be8f40c13fcde05692923efa0bddb
kamrulislam6666 <ruhan3888@gmail.com>
2025-08-24 18:51:59 +0600

    Update Bio.js

--- Script/commands/Bio.js
+++ Script/commands/Bio.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "bio",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "Tamim Tamim ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
 	description: "Change bot's bio",
 	commandCategory: "admin",
 	usages: "bio [text]",
