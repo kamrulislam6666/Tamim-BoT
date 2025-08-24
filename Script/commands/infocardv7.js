75ea3cc402900471032196cd9c38e87d8628f6a4
kamrulislam6666 <ruhan3888@gmail.com>
2025-08-24 18:41:29 +0600

    Update infocardv7.js

--- Script/commands/infocardv7.js
+++ Script/commands/infocardv7.js
@@ -4,7 +4,7 @@ module.exports.config = {
   name: "cardinfo7",
   version: "2.0.0",
   hasPermssion: 0,
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+  credits: "Tamim Tahrim ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
   description: "create card info",
   commandCategory: "info",
   cooldowns: 2,
